d91cf23128e997ad6589cf19c56fad98f1cea6ff
tab <tukhfatullin@gmail.com>
2015-03-12 21:54:25 +0600

    Add carrierwave cache folder

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,6 +1,8 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage = :fog
+    config.root = Rails.root.join('tmp')
+    config.cache_dir = 'carrierwave'
 
     config.fog_credentials = {
       :provider              => 'AWS',
@@ -10,7 +12,7 @@ CarrierWave.configure do |config|
     }
     config.fog_directory  = ENV['S3_BUCKET_NAME']
     config.fog_public     = false
-    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
+    config.fog_attributes = { 'Cache-Control' => 'max-age=315576000' }
   else
     config.storage = :file
   end
