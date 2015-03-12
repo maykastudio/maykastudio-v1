06320c6edac7754b9df8b6feee9a2ab3b0351899
tab <tukhfatullin@gmail.com>
2015-03-12 17:12:09 +0600

    RF gallery controller

--- app/controllers/manage/gallery_controller.rb
+++ app/controllers/manage/gallery_controller.rb
@@ -1,60 +1,26 @@
 class Manage::GalleryController < Manage::ApplicationController
-  before_action :set_gallery, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @collection = Gallery.order(:created_at)
-  end
-
-  def show
-  end
-
-  def new
-    @gallery = Gallery.new
-  end
+  inherit_resources
+  actions :all
 
   def create
-    @gallery = Gallery.new(gallery_params)
-
-    respond_to do |format|
-      if @gallery.save
-        format.html { redirect_to manage_gallery_path(@gallery), :notice => 'Альбом добавлен успешно!' }
-        format.json { render :show, :status => :created, :location => @gallery }
-      else
-        format.html { render :new }
-        format.json { render :json => @gallery.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  def edit
+    create!(:notice => 'Альбом добавлен успешно!')
   end
 
   def update
-    respond_to do |format|
-      if @gallery.update(gallery_params)
-        format.html { redirect_to manage_gallery_path(@gallery), :notice => 'Альбом сохранен.' }
-        format.json { render :show, :status => :ok, :location => @gallery }
-      else
-        format.html { render :edit }
-        format.json { render :json => @gallery.errors, :status => :unprocessable_entity }
-      end
-    end
+    update!(:notice => 'Альбом сохранен.')
   end
 
   def destroy
-    @gallery.destroy
-    respond_to do |format|
-      format.html { redirect_to manage_gallery_index_path, :notice => 'Альбом успешно удален!' }
-      format.json { head :no_content }
-    end
+    destroy!(:notice => 'Альбом успешно удален!') { manage_gallery_index_path }
   end
 
   private
-    def set_gallery
-      @gallery = Gallery.find(params[:id])
-    end
 
-    def gallery_params
-      params.require(:gallery).permit(:title, :description, :image, :state)
-    end
+  def collection
+    @collection = Gallery.order(:created_at)
+  end
+
+  def permitted_params
+    params.permit(:gallery => [:title, :description, :image, :state])
+  end
 end
